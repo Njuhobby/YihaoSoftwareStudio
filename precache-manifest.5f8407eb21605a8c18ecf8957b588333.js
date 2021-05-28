a2d82fcba2003bfc8c3a5177f641e282e97c5390
Njuhobby <Njuhobby@users.noreply.github.com>
2021-05-28 15:53:32 +0000

    Deploying to gh-pages from  @ 0b4e59a70c87fa5b0aa8daefa64b901bbda39235 🚀

---
+++ precache-manifest.5f8407eb21605a8c18ecf8957b588333.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a2a0ee42461ec5a0d024542686a32ab",
+    "url": "/YihaoSoftwareStudio/index.html"
+  },
+  {
+    "revision": "e801cc8756b8bc1b33db",
+    "url": "/YihaoSoftwareStudio/static/css/2.f1b50ea8.chunk.css"
+  },
+  {
+    "revision": "29b90b5da24e6b29cdc3",
+    "url": "/YihaoSoftwareStudio/static/css/3.d29f9f24.chunk.css"
+  },
+  {
+    "revision": "c5585ac5947157fc5547",
+    "url": "/YihaoSoftwareStudio/static/css/main.25b99863.chunk.css"
+  },
+  {
+    "revision": "e801cc8756b8bc1b33db",
+    "url": "/YihaoSoftwareStudio/static/js/2.fd2716f1.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/YihaoSoftwareStudio/static/js/2.fd2716f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29b90b5da24e6b29cdc3",
+    "url": "/YihaoSoftwareStudio/static/js/3.61d27c6e.chunk.js"
+  },
+  {
+    "revision": "c5585ac5947157fc5547",
+    "url": "/YihaoSoftwareStudio/static/js/main.016892a1.chunk.js"
+  },
+  {
+    "revision": "9113fe2e0cb2357a1086",
+    "url": "/YihaoSoftwareStudio/static/js/runtime-main.803ad1e7.js"
+  },
+  {
+    "revision": "a3752c4bf72688b3db38d0911cf75f61",
+    "url": "/YihaoSoftwareStudio/static/media/Agustina.a3752c4b.woff"
+  },
+  {
+    "revision": "847627bffd52aab57ce2e903f144e07f",
+    "url": "/YihaoSoftwareStudio/static/media/Montserrat-Regular.847627bf.ttf"
+  },
+  {
+    "revision": "0448a0cec05a1baa7a5a1fd3c9199678",
+    "url": "/YihaoSoftwareStudio/static/media/brownLogo.0448a0ce.png"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/YihaoSoftwareStudio/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/YihaoSoftwareStudio/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/YihaoSoftwareStudio/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/YihaoSoftwareStudio/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/YihaoSoftwareStudio/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "829ad0a1deef4d7295adfecdf12df2be",
+    "url": "/YihaoSoftwareStudio/static/media/nextuLogo.829ad0a1.webp"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/YihaoSoftwareStudio/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "25018d29d5ab115bc51c3e9a2d92f90f",
+    "url": "/YihaoSoftwareStudio/static/media/saayaHealthLogo.25018d29.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/YihaoSoftwareStudio/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/YihaoSoftwareStudio/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
