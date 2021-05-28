21c0fbad3ed748821d6017a80e8e63c486578267
yihaojiang <njuhobby@gmail.com>
2021-05-29 01:23:40 +0800

    fix bug

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -56,7 +56,7 @@ export default class Main extends Component {
           <StartupProject />
           <Achievement />
           <Blogs />
-          <Talks /> 
+          <Talks />
           <Twitter />
           <Podcast />
           <Pricing />
--- src/containers/profile/Profile.js
+++ src/containers/profile/Profile.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect, lazy, Suspense} from "react";
 import {openSource} from "../../portfolio";
 import Contact from "../contact/Contact";
 import Loading from "../loading/Loading";
+import packageJson from "../../../package.json";
 
 const renderLoader = () => <Loading />;
 const GithubProfileCard = lazy(() =>
@@ -16,7 +17,17 @@ export default function Profile() {
   useEffect(() => {
     if (openSource.showGithubProfile === "true") {
       const getProfileData = () => {
-        fetch("/profile.json")
+        const homepage = packageJson.homepage;
+        let profilePath = "";
+        let homepagePath = homepage
+          .replace("https://", "")
+          .replace("http://", "")
+          .split("/");
+        if (homepagePath.length > 1 && homepagePath[1].length > 0) {
+          profilePath = profilePath + homepagePath[1];
+        }
+        console.log(profilePath);
+        fetch(profilePath + "/profile.json")
           .then(result => {
             if (result.ok) {
               return result.json();
