365e4c7d9ca5f926b8d8deee848aca225c58d523
Njuhobby <Njuhobby@users.noreply.github.com>
2021-05-28 15:41:16 +0000

    Deploying to gh-pages from  @ c229d3de8f83070af3a0b2c3d1dd90a9040671a5 🚀

---
+++ precache-manifest.3e35bdcd5a93d32ccfba101270683c4a.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4778dee0e47085f1a70ece175cd91ac7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5a4e3b0941115dd82d62",
+    "url": "/static/css/2.f1b50ea8.chunk.css"
+  },
+  {
+    "revision": "262236b2c6e486d24b01",
+    "url": "/static/css/3.d29f9f24.chunk.css"
+  },
+  {
+    "revision": "2d17d7d7b224589b5478",
+    "url": "/static/css/main.15e5be18.chunk.css"
+  },
+  {
+    "revision": "5a4e3b0941115dd82d62",
+    "url": "/static/js/2.2dcd7a08.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/static/js/2.2dcd7a08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "262236b2c6e486d24b01",
+    "url": "/static/js/3.c39c8c46.chunk.js"
+  },
+  {
+    "revision": "2d17d7d7b224589b5478",
+    "url": "/static/js/main.a24919ee.chunk.js"
+  },
+  {
+    "revision": "74388c02ece30c218b6e",
+    "url": "/static/js/runtime-main.11e49edf.js"
+  },
+  {
+    "revision": "a3752c4bf72688b3db38d0911cf75f61",
+    "url": "/static/media/Agustina.a3752c4b.woff"
+  },
+  {
+    "revision": "847627bffd52aab57ce2e903f144e07f",
+    "url": "/static/media/Montserrat-Regular.847627bf.ttf"
+  },
+  {
+    "revision": "0448a0cec05a1baa7a5a1fd3c9199678",
+    "url": "/static/media/brownLogo.0448a0ce.png"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "829ad0a1deef4d7295adfecdf12df2be",
+    "url": "/static/media/nextuLogo.829ad0a1.webp"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "25018d29d5ab115bc51c3e9a2d92f90f",
+    "url": "/static/media/saayaHealthLogo.25018d29.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
